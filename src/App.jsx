51d4350c3c928ee310c8a514ac8f037a30a3d51e
theritalin <fatihbasacek@gmail.com>
2026-04-28 11:22:33 +0300

    mail

--- src/App.jsx
+++ src/App.jsx
@@ -164,6 +164,11 @@ function App() {
           />
         </div>
       </div>
+           <div className="footer">
+        <p><b>Sitemizde verilen bilgiler tamamen tahmini hesaplamalar olup yatırım tavsiyesi değildir.</b></p>
+        <p><b>Soru ve öneri için: ziraatbesi@gmail.com</b></p>
+
+      </div>
     </div>
   );
 }
